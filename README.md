a581d74cbfd93bf98920dfcba36445c78eb6f478
Mohammad Kheir <z5260304@ad.unsw.edu.au>
2021-09-13 01:00:31 +1000

    readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # typingSpeedInformer
 A full stack website to demonstrate my software engineering skills.
+
+# Running the pythonscraper docker server and client
+
+While in scraperpyDocker
+
+## Pythonscraper
+
+docker build -t scraperserver .
+docker run --network host -e ServerPort=6890 scraperserver 
+
+## Test Client 
+
+python3 clientTest.py
---
+++ backendjsDocker/nodeClient.js
@@ -0,0 +1,20 @@
+const net = require('net');
+const host = 'localhost';
+const port = '6890';
+const EOF = "x23Stop"
+
+const USER_ID = "not_not_mk"
+const NUMBER_OF_SCORES = "1000"
+
+var socket = new net.Socket();
+
+socket.connect(port, host, sendData());
+
+function sendData(){ socket.write(USER
+	socket.write(USER_ID + " " + NUMBER_OF_SCORES)
+}
+
+socket.on('data', (data) => {
+    console.log(`${data}`);
+    socket.destroy();
+});
